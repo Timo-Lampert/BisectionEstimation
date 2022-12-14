ce217f6c3dab3b77c0dfd911069bba03648bc585
Timo Lampert <timolampert@web.de>
2022-12-15 00:25:47 +0100

    works for non sequential functions added gap precision setting

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import decimal
 import math
 
-func = lambda x:x
+func = lambda x: x
 precision = 8
-
+gapprecision = 1  # checks for crosspoints in 1/n gaps.
+anyfound = 0
 
 def bisectionAlgorithm(x, x1):
     x2 = (x + x1) / 2
@@ -11,12 +12,48 @@ def bisectionAlgorithm(x, x1):
         x2 = (x + x1) / 2
         rootcheck = func(x) * func(x2)
         if rootcheck < 0:
+
             x1 = x2
+
         elif rootcheck > 0:
+
             x = x2
+
         elif rootcheck == 0:
             return x2
     return x2
+
+
+def calculateCrossPoints(x):
+    currentX = x / gapprecision - max
+
+    if x == 0:
+        point['x'] = currentX - 1
+        point['y'] = func(currentX - 1)
+
+    anyFound = 0
+    newPoint['y'] = func(currentX)
+    newPoint['x'] = currentX
+
+    # y = 0 between x0 and x1?
+
+    if newPoint['y'] < 0 and point['y'] >= 0 or point['y'] < 0 and newPoint['y'] >= 0 or newPoint['y'] == 0 :
+        print(f"Zero point found between {math.floor(point['x'])} and {math.ceil(newPoint['x'])}")
+        result = ""
+        global anyfound
+        anyfound = 1
+        # are the points directly at x0/x1?
+        if func(point['x']) == 0:
+            result = f"(exact match) {currentX}"
+        elif func(newPoint['x']) == 0:
+            result = f"(exact match) {currentX}"
+        else:
+            result = str(bisectionAlgorithm(point['x'], newPoint['x']))
+        print("       closest estimation " + result)
+
+    point['x'] = newPoint['x']
+    point['y'] = newPoint['y']
+
 
 
 if __name__ == '__main__':
@@ -33,30 +70,14 @@ if __name__ == '__main__':
 
     point = {'x': 0, 'y': 0}
     newPoint = {'x': 0, 'y': 0}
+    if (gapprecision > 10000 or precision > 8):
+        print("Precison set high, may take longer")
 
-    for x in range(max * 2):
-        currentX = x - max
-        if x == 0:
-            point['x'] = currentX - 1
-            point['y'] = func(currentX - 1)
 
-        newPoint['y'] = func(currentX)
-        newPoint['x'] = currentX
-
-        # y = 0 between x0 and x1?
-
-        if newPoint['y'] < 0 and point['y'] >= 0 or point['y'] < 0 and newPoint['y'] >= 0 or newPoint['y'] == 0:
-            print(f"Zero point found between {point['x']} and {newPoint['x']}")
-            result = ""
-
-            # are the points directly at x0/x1?
-            if func(point['x']) == 0:
-                result = f"(exact match) {func(point['x'])}"
-            elif func(newPoint['x']) == 0:
-                result = f"(exact match) {func(newPoint['x'])}"
-            else:
-                result = str(bisectionAlgorithm(point['x'], newPoint['x']))
-            print("       closest estimation " + result)
-
-        point['x'] = newPoint['x']
-        point['y'] = newPoint['y']
+    for x in range(max * gapprecision * 2):
+        try:
+            calculateCrossPoints(x)
+        except:
+            pass
+    if anyfound == 0:
+        print("No crosspoints found")
